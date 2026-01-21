b6e863a98d682499bcc1ae7af9de438994ca07d9
Shifa-parveen786 <shifakham0@gmail.com>
2026-01-21 15:08:20 +0530

    Delete src directory

--- src/main/java/com/project/reportcardmanagement/service/ReportCardService.java
+++
@@ -1,8 +0,0 @@
-package com.project.reportcardmanagement.service;
-
-import com.project.reportcardmanagement.model.ReportCard;
-
-public interface ReportCardService {
-
-    ReportCard generateReportCard(String studentId) throws Exception;
-}
